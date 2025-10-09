0cadfad7166f485403029af8ea1dd46373843847
Elisame Araújo <elisamearaujo@gmail.com>
2025-10-09 17:43:45 -0300

    [atualizado] adicionado aliases para as classes de helpers

--- config/app.php
+++ config/app.php
@@ -123,4 +123,15 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Aliases
+    |--------------------------------------------------------------------------
+    */
+    'aliases' => [
+        'Utilities' => App\Helpers\Utilities::class,
+        'DateHelper' => App\Helpers\DateHelper::class
+    ],
+
+
 ];

1ef0c6110643ee32624c19e18ba0b5792bfb2daf
Elisame Araújo <elisamearaujo@gmail.com>
2025-10-11 15:48:43 -0300

    [corrigido] preview de upload de imagem

--- resources/js/admin/fileUploadImagePreview.js
+++ resources/js/admin/fileUploadImagePreview.js
@@ -1,28 +1,37 @@
 export function fileUploadPreview() {
-    const uploadButtons = document.querySelectorAll("[data-label]");
+    const overviewInputs = document.querySelectorAll("input[type='file'][data-overview]");
+    const galleryInputs = document.querySelectorAll("input[type='file'][data-gallery]");
 
-    uploadButtons.forEach((uploadButton) => {
-        uploadButton.addEventListener("change", (e) => {
-            const currFiles = e.target.files;
+    const setupPreview = (input) => {
+        const container = input.closest(".update-image-area");
+        const changeButton = container.querySelector("#change-image");
+        const currentImageContainer = container.querySelector(".current-image");
+        const imagePreview = container.querySelector("#preview");
+        const imagePreviewContainer = container.querySelector(".image-preview");
+        const uploadButtonContainer = container.querySelector(".upload-new-image");
 
-            const text = document.getElementById("text");
-            if (text) text.remove();
-
-            if (currFiles.length > 0) {
-                const src = URL.createObjectURL(currFiles[0]);
-
-                const preview = document.getElementById("preview");
-                const imagePreview = document.getElementById("file-preview");
-
-                if (imagePreview) {
-                    imagePreview.src = src;
-                    imagePreview.style.display = "block";
-                }
-
-                if (preview) {
-                    preview.classList.remove("hidden");
-                }
-            }
+        changeButton.addEventListener("click", (e) => {
+            e.preventDefault();
+            input.click();
         });
-    });
+
+        input.addEventListener("change", (e) => {
+            const file = e.target.files[0];
+            if (!file) return;
+
+            // Limpa o conteúdo da área de imagem atual
+            if (currentImageContainer) {
+                currentImageContainer.innerHTML = "";
+            }
+
+            // Atualiza preview
+            const src = URL.createObjectURL(file);
+            imagePreview.src = src;
+            imagePreview.style.display = "block";
+            imagePreviewContainer.classList.remove("hidden");
+            uploadButtonContainer.classList.remove("hidden");
+        });
+    };
+
+    [...overviewInputs, ...galleryInputs].forEach(setupPreview);
 }

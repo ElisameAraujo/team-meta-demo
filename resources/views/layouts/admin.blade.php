f735edc713ecffd0a0e6c6a71d8806a2f773df31
Elisame Araújo <elisamearaujo@gmail.com>
2025-10-30 19:27:27 -0300

    [atualizado] removido o icone de página que não existe

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Administration | @yield('title')</title>
-    <link rel="shortcut icon" href="{{ asset('img/svg/favicon.svg') }}" type="image/x-icon" />
     @vite(['resources/css/admin.css', 'resources/js/admin.js'])
 </head>
 

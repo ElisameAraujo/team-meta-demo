8fdc44ab73125ef13540712900cf3e69fb8b45f7
Elisame Araújo <elisamearaujo@gmail.com>
2025-10-31 12:48:19 -0300

    [corrigido] um erro que poderia gerar a chave `to_key` incompleta e impedir a transição de ser executada

--- resources/views/components/admin/new-transition.blade.php
+++ resources/views/components/admin/new-transition.blade.php
@@ -1,6 +1,6 @@
 <dialog id="new_transition" class="modal">
     <div class="modal-box">
-        <form x-data="{ fromKey: '' }" method="post" enctype="multipart/form-data"
+        <form x-data="{ fromKey: '', toKey: '' }" method="post" enctype="multipart/form-data"
             action="{{ route('admin.transitions.add-transition') }}">
             @csrf
             <h1 class="text-xl">Add Transition</h1>
@@ -9,22 +9,40 @@
                 <legend class="fieldset-legend">From</legend>
                 <select class="select w-full" name="from_key" x-model="fromKey">
                     <option disabled selected value="">-- Select an Section --</option>
-                    <option value="zoom-in">Zoom In</option>
-                    <option value="zoom-out">Zoom Out</option>
-                    @foreach ($sections as $s)
-                        <option value="{{ $s->section_slug }}">{{ $s->section_name }}</option>
+                    @foreach ($sections as $section)
+                        <option value="{{ $section->section_slug }}">
+                            {{ $section->section_name }}
+                        </option>
+                    @endforeach
+                    @foreach ($sections as $section)
+                        <option value="zoom-in-{{ $section->section_slug }}">
+                            Zoom In ({{ $section->section_name }})
+                        </option>
+                        <option value="zoom-out-{{ $section->section_slug }}">
+                            Zoom Out ({{ $section->section_name }})
+                        </option>
                     @endforeach
                 </select>
             </fieldset>
 
             <fieldset class="fieldset">
                 <legend class="fieldset-legend">To</legend>
-                <select class="select w-full" name="to_key">
-                    <option value="zoom-in">Zoom In</option>
-                    <option value="zoom-out">Zoom Out</option>
-                    @foreach ($sections as $s)
-                        <option value="{{ $s->section_slug }}" :disabled="fromKey === '{{ $s->section_slug }}'">
-                            {{ $s->section_name }}
+                <select class="select w-full" name="to_key" x-model="toKey">
+                    @foreach ($sections as $section)
+                        <option value="{{ $section->section_slug }}"
+                            :disabled="fromKey === '{{ $section->section_slug }}'">
+                            {{ $section->section_name }}
+                        </option>
+                    @endforeach
+                    @foreach ($sections as $section)
+                        <option value="zoom-in-{{ $section->section_slug }}"
+                            :disabled="fromKey === 'zoom-in-{{ $section->section_slug }}'">
+                            Zoom In ({{ $section->section_name }})
+                        </option>
+
+                        <option value="zoom-out-{{ $section->section_slug }}"
+                            :disabled="fromKey === 'zoom-out-{{ $section->section_slug }}'">
+                            Zoom Out ({{ $section->section_name }})
                         </option>
                     @endforeach
                 </select>
